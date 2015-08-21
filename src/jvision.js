1d8914932e81b66ef3eacc47ee63199d689874a6
REZ1DENT3 <maksim.babichev95@gmail.com>
2015-08-22 02:40:20 +0300

    Fixed + new fiches

--- src/jvision.js
+++ src/jvision.js
@@ -4,6 +4,24 @@ if (typeof String.prototype.trim == 'undefined') {
     };
 }
 
+if (!window.requestAnimationFrame) {
+
+    window.requestAnimationFrame = (function () {
+
+        return window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback, element) {
+
+                window.setTimeout(callback, 1000 / 60);
+
+            };
+
+    })();
+
+}
+
 (function () {
 
     var jVObject = function (selector) {
@@ -12,6 +30,10 @@ if (typeof String.prototype.trim == 'undefined') {
 
         this.push = function (newDom) {
             this.treeDom.push(newDom);
+        };
+
+        this.setTreeDom = function (value) {
+            this.treeDom = value;
         };
 
         switch (typeof selector) {
@@ -28,6 +50,7 @@ if (typeof String.prototype.trim == 'undefined') {
                         case '[object NodeList]':
                             this.treeDom = selector;
                             break;
+                        case '[object HTMLLIElement]':
                         case '[object HTMLParagraphElement]':
                         default:
                             this.push(selector);
@@ -60,16 +83,22 @@ if (typeof String.prototype.trim == 'undefined') {
         };
 
         this.first = function () {
-            return new jVObject(this.at(0));
+            return this.eq(0);
         };
 
         this.last = function () {
-            return new jVObject(this.at(-1));
+            return this.eq(-1);
         };
 
         this.each = function (callback) {
             for (var i = 0; i < this.count(); ++i) {
                 callback(this.at(i), i);
+            }
+        };
+
+        this.call = function (callback) {
+            for (var i = 0; i < this.count(); ++i) {
+                callback(this.eq(i), i);
             }
         };
 
@@ -125,12 +154,34 @@ if (typeof String.prototype.trim == 'undefined') {
             return null;
         };
 
-        this.css = function (type, value) {
-            if (typeof value == 'undefined') {
-                return node.style[type];
+        this.attr = function (type, value) {
+            if (this.count() == 1) {
+                if (typeof value == 'undefined') {
+                    if (!this.at(0).hasAttribute(type)) {
+                        return null;
+                    }
+                    return this.at(0).getAttribute(type);
+                }
+                else if (value == null) {
+                    this.at(0).removeAttribute(type);
+                }
+                else {
+                    this.at(0).setAttribute(type, value);
+                }
+                return this;
             }
             this.each(function (node) {
-                node.style[type] = value;
+                node.setAttribute(type, value);
+            });
+            return null;
+        };
+
+        this.css = function (type, value) {
+            if (typeof value == 'undefined' && this.count() == 1) {
+                return this.at(0).style.getPropertyValue(type);
+            }
+            this.each(function (node) {
+                node.style.setProperty(type, value);
             });
             return this;
         };
@@ -158,7 +209,7 @@ if (typeof String.prototype.trim == 'undefined') {
 
         this.hasClass = function (className) {
             if (this.count() == 1) {
-                return this.eq(0).classList.contains(className);
+                return this.at(0).classList.contains(className);
             }
             return null;
         };
@@ -178,18 +229,60 @@ if (typeof String.prototype.trim == 'undefined') {
         };
 
         this.find = function (selector) {
-            var newObject = new jVObject(this.treeDom);
             var list = [];
-            newObject.each(function (node) {
+            this.each(function (node) {
                 list.push(node.querySelectorAll(selector));
             });
+
+            var newObject = new jVObject();
             if (list.length == 1) {
-                newObject.treeDom = list[0];
+                // todo
+                newObject.setTreeDom(list[0]);
             }
             else {
-                newObject.treeDom = list;
+                // todo
+                newObject.setTreeDom(list);
             }
             return newObject;
+        };
+
+        this.animate = function (event, duration, complite) {
+            var start = performance.now();
+            requestAnimationFrame(function animate(time) {
+                var timePassed = time - start;
+                if (timePassed > duration) {
+                    timePassed = duration;
+                }
+                event(timePassed, start, time, duration);
+                if (timePassed < duration) {
+                    requestAnimationFrame(animate);
+                }
+                else if (typeof complite == 'function') {
+                    complite();
+                }
+            });
+        };
+
+        // todo
+        this.hide = function (timer) {
+            var obj = this;
+            this.animate(function (timePassed, start, time, duration) {
+                obj.css('opacity', 1 - Math.abs(timePassed) / Math.abs(duration));
+            }, timer, function () {
+                obj.css('display', 'none').css('opacity', 1);
+            });
+            return this;
+        };
+
+        this.show = function (timer) {
+            var obj = this;
+            obj.css('display', 'block');
+            this.animate(function (timePassed, start, time, duration) {
+                obj.css('opacity', Math.abs(timePassed) / Math.abs(duration));
+            }, timer, function () {
+                obj.css('opacity', 1);
+            });
+            return this;
         };
 
     };
@@ -205,7 +298,7 @@ if (typeof String.prototype.trim == 'undefined') {
         };
 
         this.ready = function (callback) {
-            document.addEventListener("DOMContentLoaded", callback);
+            window.addEventListener("DOMContentLoaded", callback);
             return this;
         };
 
